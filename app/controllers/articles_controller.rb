7f239a30901a33af620b600e95b984c47bf8bfee
Sinan KILIÇ <sinankilic61@gmail.com>
2020-08-14 12:15:01 +0000

    Update function added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #render plain: params[:article]
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -22,6 +26,17 @@ class ArticlesController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render "edit"
+    end
+
   end
 
 end
