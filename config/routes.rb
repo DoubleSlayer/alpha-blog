e262c0b5c20a7c0500daad25bb543bb58825cf10
Sinan KILIÇ <sinankilic61@gmail.com>
2020-08-17 14:58:17 +0000

    Adeed delete and some action functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
 end
